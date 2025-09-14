c1d9d248721d684006322a84c96d7776ab20caef
Jesse-Calebe <jcalebe18@gmail.com>
2025-09-14 15:41:43 -0300

    Enhance Cockpit application layout and navigation functionality
    - Refactor Component.js to register module paths for Clientes and Produtos.
    - Update Cockpit.controller.js to implement navigation item selection and toggle expand functionality.
    - Revise Cockpit.view.xml to include SideNavigation and toggle button for improved user interaction.

--- app/cockpit/webapp/Component.js
+++ app/cockpit/webapp/Component.js
@@ -1,26 +1,34 @@
-sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "comp/container/com/cockpit/model/models"
-], (UIComponent, models) => {
+sap.ui.define(
+  ["sap/ui/core/UIComponent", "comp/container/com/cockpit/model/models"],
+  (UIComponent, models) => {
     "use strict";
 
     return UIComponent.extend("comp.container.com.cockpit.Component", {
-        metadata: {
-            manifest: "json",
-            interfaces: [
-                "sap.ui.core.IAsyncContentCreation"
-            ]
-        },
+      metadata: {
+        manifest: "json",
+        interfaces: ["sap.ui.core.IAsyncContentCreation"],
+      },
 
-        init() {
-            // call the base component's init function
-            UIComponent.prototype.init.apply(this, arguments);
+      init() {
+        // call the base component's init function
+        UIComponent.prototype.init.apply(this, arguments);
 
-            // set the device model
-            this.setModel(models.createDeviceModel(), "device");
+        // set the device model
+        this.setModel(models.createDeviceModel(), "device");
 
-            // enable routing
-            this.getRouter().initialize();
-        }
+        // enable routing
+        this.getRouter().initialize();
+            
+        // register module paths
+        jQuery.sap.registerModulePath(
+          "comp.container.com.clientes",
+          "../../clientes/webapp"
+        );
+        jQuery.sap.registerModulePath(
+          "comp.container.com.produtos",
+          "../../produtos/webapp"
+        );
+      },
     });
-});
+  }
+);
--- app/cockpit/webapp/controller/Cockpit.controller.js
+++ app/cockpit/webapp/controller/Cockpit.controller.js
@@ -1,10 +1,50 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/ComponentContainer"],
+  (Controller, ComponentContainer) => {
     "use strict";
 
+    const COMP_MAP = {
+      produtosKey: {
+        name: "comp.container.com.produtos",
+        manifest: true,
+        component: "produtos",
+        width: "100%",
+        height: "100%",
+      },
+      clientesKey: {
+        name: "comp.container.com.clientes",
+        manifest: true,
+        component: "clientes",
+        width: "100%",
+        height: "100%",
+      },
+    };
+
     return Controller.extend("comp.container.com.cockpit.controller.Cockpit", {
-        onInit() {
-        }
+      onInit() {},
+      onToggleExpand(oEvent) {
+        let oSideNav = this.byId("sideNavigation");
+
+        oSideNav.setExpanded(!oSideNav.getExpanded());
+
+        let oButton = oEvent.getSource();
+
+        oButton.setIcon(
+          oSideNav.getExpanded()
+            ? "sap-icon://close-command-field"
+            : "sap-icon://open-command-field"
+        );
+      },
+      onNavItemSelect(oEvent) {
+        let oItem = oEvent.getParameter("item");
+        let sNavSelectedKey = oItem.getKey();
+
+        let oContentVbox = this.byId("contentVBox");
+        oContentVbox.destroyItems();
+
+        let oCompContainer = new ComponentContainer(COMP_MAP[sNavSelectedKey]);
+        oContentVbox.addItem(oCompContainer);
+      },
     });
-});
+  }
+);
