92e5eb2cf0eb3060ea176c80d22b62ada6704038
Jessé Silva <jcalebe18@gmail.com>
2025-09-14 21:25:28 +0000

    fixing issues for deploy

--- app/cockpit/webapp/Component.js
+++ app/cockpit/webapp/Component.js
@@ -18,16 +18,48 @@ sap.ui.define(
 
         // enable routing
         this.getRouter().initialize();
-            
-        // register module paths
-        jQuery.sap.registerModulePath(
-          "comp.container.com.clientes",
-          "../../clientes/webapp"
-        );
-        jQuery.sap.registerModulePath(
-          "comp.container.com.produtos",
-          "../../produtos/webapp"
-        );
+
+
+
+        // register module paths - Begin
+        // Workzone   : this._componentConfig.url
+        // Standalone : location.pathname
+        let aPathLocation = this._componentConfig.url ? this._componentConfig.url.split('.') : location.pathname.split('.');
+
+        const APPS_TO_EXPOSE = [
+          { id: "comp.container.com.clientes", version: "0.0.1" },
+          { id: "comp.container.com.produtos", version: "0.0.1" },
+        ]
+
+        if (aPathLocation.length >= 2) {
+          let sDestination = aPathLocation[0]
+          let sRouter = aPathLocation[1]
+
+          APPS_TO_EXPOSE.forEach(element => {
+            jQuery.sap.registerModulePath(
+              element.id,
+              `${sDestination}.${sRouter}.${element.id.replaceAll('.', '')}-${element.version}`
+            );
+          });
+        };
+        // register module paths - End
+
+        // register module paths for LOCAL TEST - Begin
+        if (location.host.includes('applicationstudio') || location.host.includes('localhost')) {
+          const APPS_TO_EXPOSE_LOCAL = [
+            { id: "comp.container.com.clientes", path: "../../clientes/webapp" },
+            { id: "comp.container.com.produtos", path: "../../produtos/webapp" },
+          ]
+
+          APPS_TO_EXPOSE_LOCAL.forEach(element => {
+            jQuery.sap.registerModulePath(
+              element.id,
+              element.path
+            );
+          });
+        }
+        // register module paths for LOCAL TEST - End
+
       },
     });
   }
